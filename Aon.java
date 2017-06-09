5af743b29599ef1983f6603cd5d4979e2cd03322
Mohanapriya20 <mohanagesan97@gmail.com>
2017-06-09 15:58:34 +0530

    Create Aon.java

---
+++ Aon.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.lang.*;
+class Aon
+{
+ public static void main(String args[])
+{
+  char an='*';
+  if((an>='a' && an<='z') || (an>='A' && an<='Z'))
+{
+System.out.println("given char is alphabet"+" "+an);
+}
+else 
+{
+System.out.println("given char is not alphabet"+" "+an);
+}
+}
+}
